8abfd7e02b0f02e90ba2f78a4be0808f5d0fb92b
prachijain2508 <prachijain2508@gmail.com>
2025-12-31 15:54:28 +0530

    Add asociations with nested attributes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :quiz
+  has_many :options, dependent: :destroy
+
+  accepts_nested_attributes_for :options, allow_destroy: true
+
+  QUESTION_TYPES = %w[mcq true_false text].freeze
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,2 +1,5 @@
 class Quiz < ApplicationRecord
+  has_many :questions, dependent: :destroy
+
+  accepts_nested_attributes_for :questions, allow_destroy: true
 end
